12006b32ab5bab4b1360e1011aa22dbee7b2a5c7
Hari-184 <hm2592@srmist.edu.in>
2026-03-12 14:57:38 +0530

    [main] Added [UC1 - Application Entry & Welcome Message]

---
+++ App/src/palindromecheckerapp.java
@@ -0,0 +1,15 @@
+public class palindromecheckerapp {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+
+
+        System.out.println("Version : 1.0");
+
+
+        System.out.println("System initialized successfully.");
+    }
+}
+
